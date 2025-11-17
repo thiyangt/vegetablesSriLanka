743c1d3c65f5ccdbf564ca5b4669ef2c515b2b28
thiyangt <tstalagala@gmail.com>
2025-11-17 18:39:41 +0530

    updated

--- data-raw/pkgwebandhex.R
+++ data-raw/pkgwebandhex.R
@@ -10,18 +10,20 @@ pkgdown::build_site()
 
 ## Hex sticker
 library(hexSticker)
-pic <- here::here("data-raw", "veg.png")
+pic <- here::here("logo", "veg.png")
 sticker(
   subplot = pic,                 # your image file
-  package = "vegetablesSriLanka",       # package name text
-  p_size = 14,                         # text size
+  package = "vegscraper",       # package name text
+  p_size = 20,                         # text size
   p_color = "white",                   # text color
+  p_family = "bold",
+  p_fontface = "bold",
   p_y = 1.0,                           # vertical position of text
   s_x = 1, s_y = 1,                 # position of image inside hex
   s_width = 0.6, s_height = 0.6,       # size of image within hex
   h_fill = "#2E8B57",                  # hex background fill (green tone)
   h_color = "#ffffff",                 # border color
-  filename = "vegetableSriLanka_hex.png",
+  filename = here::here("logo", "hex.png"),
   dpi = 300
 )
 
